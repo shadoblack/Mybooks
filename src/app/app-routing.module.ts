e088ee601a76debaf6e1dc73ca0f4edde07eb2e2
shadoblack <edgarerb@hotmail.es>
2022-03-11 11:38:57 +0100

    commit dia 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { FooterComponent } from './component/footer/footer.component';
+import { HomeComponent } from './pages/home/home.component';
+import { FormularioRegistroComponent } from './component/formulario-registro/formulario-registro.component';
+import { RegistroComponent } from './pages/registro/registro.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "formulario-registro", component: FormularioRegistroComponent },
+  {path: "AppComponent", component: AppComponent},
+  {path: "HeaderComponent", component: HeaderComponent},
+  {path: "FooterComponent", component: FooterComponent},
+  {path: "HomeComponent", component: HomeComponent},
+  {path: "RegistroComponent", component: RegistroComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
