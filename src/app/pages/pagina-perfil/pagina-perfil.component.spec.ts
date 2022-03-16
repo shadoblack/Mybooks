9bb604634fdcf6bfc27a3a05b7d7f9b05df9550a
shadoblack <edgarerb@hotmail.es>
2022-03-16 10:11:11 +0100

    fin dia 2

---
+++ src/app/pages/pagina-perfil/pagina-perfil.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PaginaPerfilComponent } from './pagina-perfil.component';
+
+describe('PaginaPerfilComponent', () => {
+  let component: PaginaPerfilComponent;
+  let fixture: ComponentFixture<PaginaPerfilComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PaginaPerfilComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(PaginaPerfilComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
